b367a976bbd4f23eee330463237f661fdbca214c
danieltorressierra2002 <danieltorressierra2002@gmail.com>
2026-09-03 00:20:04 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-white">
+      <h1 className="text-3xl font-bold">Bodegón La Fé 🛒</h1>
+    </div>
+  )
+}
