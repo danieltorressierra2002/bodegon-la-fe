4ab35c58c26e149e95ad4e3e8450a648b158bfba
danieltorressierra2002 <danieltorressierra2002@gmail.com>
2026-09-03 00:15:37 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        naranja: "#FF6B00",
+        neon: "#FFD700",
+      },
+    },
+  },
+  plugins: [],
+}
