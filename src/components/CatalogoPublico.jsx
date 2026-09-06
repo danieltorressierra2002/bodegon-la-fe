904645489b88693aa86db1ee67f0581ea6b69fa5
danieltorressierra2002 <danieltorressierra2002@gmail.com>
2026-09-06 14:35:01 -0400

    Update CatalogoPublico.jsx

--- src/components/CatalogoPublico.jsx
+++ src/components/CatalogoPublico.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { supabase } from "../lib/supabase"
-import { obtenerCarrito, agregarAlCarrito, calcularTotal } from "./lib/carrito"
+import { obtenerCarrito, agregarAlCarrito, calcularTotal } from "../lib/carrito"
 
 export default function CatalogoPublico() {
   const [categorias, setCategorias] = useState([])
